6a2b493c386ff472145365768f64b86b6df63d18
Neill Boyd <neill.boyd@instil.co>
2016-04-28 16:50:04 +0100

    Updating jade structure and adding in more templates

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , template = require('jade').compileFile(__dirname + '/views/water.jade')
 
 app.use(logger('dev'))
+app.set('view engine', 'jade')
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
     var html = template({ title: 'Home' })
     res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/credit', function (req, res, next) {
+  try {
+    res.render('credit')
   } catch (e) {
     next(e)
   }
