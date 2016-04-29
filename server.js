238641e2e2cab34f868b0643b99a2f4ab8f7e266
Neill Boyd <neill.boyd@instil.co>
2016-04-29 10:36:11 +0100

    Adding basic authentication to /login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
+  , auth = require('basic-auth')
   , template = require('jade').compileFile(__dirname + '/views/water.jade')
 
 app.use(logger('dev'))
@@ -22,6 +23,23 @@ app.get('/credit', function (req, res, next) {
   } catch (e) {
     next(e)
   }
+})
+
+app.get('/login', function(req, res, next){
+  try {
+    credentials = auth(req);
+    if (!credentials || credentials.name !=='admin' || credentials.pass !== 'password')
+    {
+      res.statusCode = 401
+      res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+      res.end('Access denied')
+    }
+    else
+    {
+      res.end('Access Granted!')
+    }
+  } catch (e) {
+  next(e)}
 })
 
 app.listen(process.env.PORT || 3000, function () {
